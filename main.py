1de523fa95eee6ef41db865cf6f2ec5b6e27d7d2
Dominick Rose <Dom.Rose42@gmail.com>
2021-05-27 14:16:18 -0600

    Added logging to file

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from flask import Flask
 
 from routes.routes import client_routes, account_routes
 
+import logging
+
 app = Flask(__name__)
+logging.basicConfig(filename='record.log', level=logging.DEBUG, format = f'%(asctime)s %(levelname)s %(message)s')
 
 client_routes(app)
 account_routes(app)
